67f86550700c29e858a4bfbb280f829249876852
programmerolshow <olanrewajuillias@gmail.com>
2026-05-19 11:56:56 +0100

    Fixed border of ConfirmProvider

--- src/context/ConfirmProvider.tsx
+++ src/context/ConfirmProvider.tsx
@@ -118,7 +118,7 @@ const ConfirmProvider = ({ children }: { children: ReactNode }) => {
               <button
                 onClick={close}
                 disabled={loading}
-                className={`md:px-8 md:py-3 px-4 py-2 rounded text-md border-[2px] border-[#0b4f13] disabled:opacity-50 cursor-pointer 
+                className={`md:px-8 md:py-3 px-4 py-2 rounded text-md border-2 border-[#0b4f13] disabled:opacity-50 cursor-pointer 
                 `}
               >
                 {options.cancelText ?? "Cancel"}
