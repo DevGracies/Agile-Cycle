cf9848d642b053b2f947220a89cc1eef1f9f4977
Emmanuel <okoosiemmanuel@gmail.com>
2026-06-19 19:36:23 +0100

    fix: resolve duplicate component import

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -4,11 +4,6 @@ import FeaturesSection from "@/src/components/sections/FeatureBanner";
 import SubscribeSection from "@/src/components/sections/SubscribeSection";
 import { ReactNode } from "react";
 
-import Navbar from "@/src/components/userBars/Navbar";
-import Footer from "@/src/components/layout/Footer";
-import SubscribeSection from "@/src/components/sections/SubscribeSection";
-import FeaturesSection from "@/src/components/sections/FeatureBanner";
-
 export default function PublicLayout({
   children,
 }: {
