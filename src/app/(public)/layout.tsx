cce1653570846ae37c50f91fe1b6a00ffe7e0235
samuel-aigbefo <samuelehigale@gmail.com>
2026-07-10 16:56:05 +0100

    updated app/public/layout

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -2,7 +2,6 @@ import Footer from "@/src/components/layout/Footer";
 import Navbar from "@/src/components/layout/Navbar";
 import FeaturesSection from "@/src/components/sections/FeatureBanner";
 import SubscribeSection from "@/src/components/sections/SubscribeSection";
-import { ReactNode } from "react";
 
 export default function PublicLayout({
   children,
@@ -13,7 +12,9 @@ export default function PublicLayout({
     <>
       <div>
         <Navbar />
-        {children}
+         <main className="pt-16">
+          {children}
+         </main>
         <SubscribeSection />
         <FeaturesSection />
         <Footer />
